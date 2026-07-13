ec202cd51e92d2f414420dba83a43ce470b4e740
lingkas <lingkas48@gmail.com>
2026-07-13 17:41:48 +0700

    Add files via upload

---
+++ data/KantorPemerintahan_3.js
@@ -0,0 +1,23 @@
+var json_KantorPemerintahan_3 = {
+"type": "FeatureCollection",
+"name": "KantorPemerintahan_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.387381475000041, -7.787114150999969 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "8", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Wali Kota", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.391178470000057, -7.799838273999967 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.380139005000046, -7.801648734999958 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.377712138000049, -7.808404501999973 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.387674540000035, -7.809793996999929 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.39527120300005, -7.817955059999974 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.355798715000049, -7.783071399999926 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.362371176000067, -7.783302753999976 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.358602413000085, -7.789302563999968 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "7", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Gubernur", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.366001235000056, -7.79402737199996 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.373395479000067, -7.794690526999943 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.365808793000042, -7.802932711999972 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.362709401000075, -7.805584780999936 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.35573140300005, -7.807398694999961 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.348007340000038, -7.810633674999963 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": null, "FGSGOV": "9", "LUAS": 0.0, "FCODE": null, "REMARK": "Kantor Camat", "SRS_ID": null, "LCODE": "GD0020", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 110.361188502000061, -7.821399725999981 ] } }
+]
+}
